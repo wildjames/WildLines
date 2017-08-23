40bf4ea8ec4c43a0936133c8722e416b86f02f35
WizenedChimp <wildo543@hotmail.com>
2017-08-23 12:00:56 +0100

    Bugfixes and new functionality

--- LinesPlotLib.py
+++ LinesPlotLib.py
@@ -16,7 +16,7 @@ def setup_plot(title=None, wr=20, figsize=[3.15, 4], pad=0.15):
     
     fig, ax = plt.subplots(figsize=figsize)
     if title != None:
-        fig.subplots_adjust(left=pad, right=0.95, top=0.94, bottom=0.12)
+        fig.subplots_adjust(left=pad, right=0.95, top=0.90, bottom=0.15)
         plt.title(title)
     else:
         fig.subplots_adjust(left=pad, right=0.95, top=0.97, bottom=0.12)
@@ -25,15 +25,16 @@ def setup_plot(title=None, wr=20, figsize=[3.15, 4], pad=0.15):
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
     plt.xlabel(r'Wavelength ($\mathrm{\AA}$)', fontsize=11)
     plt.ylabel('Normalised Flux', fontsize=11)
-    ax.tick_params('both', length=10, width=1, which='minor', direction='inout', top='on', bottom='on', 
+    ax.tick_params('both', length=10, width=1, which='minor', direction='inout', top='on', bottom='on',
         labelright='off', labelleft='on', labelbottom='on', labeltop='off')
-    ax.tick_params('both', length=5,  width=1, which='major', direction='inout', top='on', bottom='on', 
+    ax.tick_params('both', length=5,  width=1, which='major', direction='inout', top='on', bottom='on',
         labelright='off', labelleft='on', labelbottom='on', labeltop='off')
     ax.axvline(x=0.0, alpha=0.3, color='black')
     ax.set_xlim(0-wr, wr)
     return fig, ax
 
-def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=None, offset=0.5, text_shift=0.2, out='n', figsize=[3.15,4], pad=0.15, v=0.0):
+def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=None, offset=0.5, 
+    text_shift=0.2, out='n', figsize=[3.15,4], pad=0.15, v=0.0, col1=0, col2=1, col3=None, col4=None):
 # Generate the plots.
     ### USAGE:
     ##  <lines>      - list of lines to plot
@@ -49,9 +50,9 @@ def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=Non
     
     # Grab the data from the file
     if type(fname) == str:
-        obs = get_data(fname, v)
+        obs = get_data(fname, v, col1, col2, col3, col4)
     elif type(fname) == list:
-        obs = get_mult_data(fname, v)
+        obs = get_mult_data(fname, v, col1, col2, col3, col4)
     else:
         print 'Invalid spectrum filename input!! %s ' % type(fname)
 
@@ -60,10 +61,12 @@ def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=Non
     wl   = obs['wl']
     fobs = obs['fobs']
     fgen = obs['fgen']
+    fgen2= obs['fgen2']
 
     pwl = []              #wl to plot
     pfo = []              #obs to plot
     pfg = []              #gen to plot
+    pfh = []              #Optional 4th line
     wmax, wmin = 0.,0.    #Range boundaries
     s = 0.                #Initial offset.
     n = 1                 #counter
@@ -80,24 +83,50 @@ def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=Non
     else:
         # Ensure that the labels are all strings.
         labels = [str(i) for i in labels]
+
     # scan through the list of lines and pull out sections about the wavelengths we want.
     for i, label in zip(lines, labels):
         # Reset variables
         pwl = []
         pfo = []
         pfg = []
+        pfh = []
         wmax = i+wr
         wmin = i-wr
-        # search the wavelength lists
-        for j,k,l in zip(wl, fobs, fgen):
-            if j > wmin and j < wmax:
-                pwl.append(j-i)
-                pfo.append(k+s)
-                pfg.append(l+s)
-                if pfo[-1] < fmin:
-                    fmin = pfo[-1]
-        plt.step(pwl, pfo, color='black')
-        plt.plot(pwl, pfg, color='red')
+        # search the wavelength lists.
+        #######################################
+        ### This is hideous code. FIX IT!!! ###
+        #######################################
+        if col4 != None:
+            for j,k,l,m in zip(wl, fobs, fgen, fgen2):
+                if j > wmin and j < wmax:
+                    pwl.append(j-i)
+                    pfo.append(k+s)
+                    pfg.append(l+s)
+                    pfh.append(m+s)
+                    if pfo[-1] < fmin:
+                        fmin = pfo[-1]
+            plt.step(pwl, pfo, color='black')
+            plt.plot(pwl, pfg, color='red')
+            plt.plot(pwl, pfh, color='blue')
+        elif col3 != None:
+            for j,k,l in zip(wl, fobs, fgen):
+                if j > wmin and j < wmax:
+                    pwl.append(j-i)
+                    pfo.append(k+s)
+                    pfg.append(l+s)
+                    if pfo[-1] < fmin:
+                        fmin = pfo[-1]
+            plt.step(pwl, pfo, color='black')
+            plt.plot(pwl, pfg, color='red')
+        else:
+            for j,k in zip(wl, fobs):
+                if j > wmin and j < wmax:
+                    pwl.append(j-i)
+                    pfo.append(k+s)
+                    if pfo[-1] < fmin:
+                        fmin = pfo[-1]
+            plt.step(pwl, pfo, color='black')
 
         plt.annotate(label,
                       xy=(text_pos,1.1+s),
@@ -124,19 +153,23 @@ def generate_plots(lines, fname, wr=20, title=None, oname='out', N=6, labels=Non
             plt.savefig(oname+str(number)+'.pdf')
     plt.close()
 
-def get_data(fname, v):
+def get_data(fname, v=0.0, col1=0, col2=1, col3=None, col4=None):
 # Read in the data, return dict of data
     wl = []
     fobs = []
     fgen = []
+    fgen2 = []
     with open(fname, 'r') as f:
         for i in range(3):
             f.readline()
         for line in f:
             line = [float(x) for x in line.split()]
-            wl.append(line[0])
-            fobs.append(line[1])
-            fgen.append(line[3])
+            wl.append(line[col1])
+            fobs.append(line[col2])
+            if col3 != None:
+                fgen.append(line[col3])
+            if col4 != None:
+                fgen2.append(line[col4])
     # velocity correction
     c = 3e5
     wl = [x*(1-(v/c)) for x in wl]
@@ -144,15 +177,17 @@ def get_data(fname, v):
     #Store data in a dict
     obs = {'wl': wl,
            'fobs': fobs,
-           'fgen': fgen
+           'fgen': fgen,
+           'fgen2': fgen2
            }
     return obs
 
-def get_mult_data(fnames, v):
+def get_mult_data(fnames, v=0.0, col1=0, col2=1, col3=None, col4=None):
 # Read in multiple files, and return a dict of the data
     wl = []
     fobs = []
     fgen = []
+    fgen2 = []
     # Loop over the filenames, and append the data to the same lists
     for fname in fnames:
         with open(fname, 'r') as f:
@@ -160,9 +195,12 @@ def get_mult_data(fnames, v):
                 f.readline()
             for line in f:
                 line = [float(x) for x in line.split()]
-                wl.append(line[0])
-                fobs.append(line[1])
-                fgen.append(line[3])
+                wl.append(line[col1])
+                fobs.append(line[col2])
+                if col3 != None:
+                    fgen.append(line[col3])
+                if col4 != None:
+                    fgen2.append(line[col4])
 
     # velocity correction
     c = 3e5
@@ -171,6 +209,7 @@ def get_mult_data(fnames, v):
     #Store data in a dict
     obs = {'wl': wl,
            'fobs': fobs,
-           'fgen': fgen
+           'fgen': fgen,
+           'fgen2': fgen2
            }
     return obs
--- plotSpecLines.py
+++ plotSpecLines.py
@@ -18,11 +18,16 @@ class lplinterp():
             ##  [text_shift] - fractional shift inwards from the negative x axis limit
             ##  [out]        - y/n do we print to file? Default no.
             # Retrieve observations and model
+        self.echo       = False
         self.lines      = []
         self.fname      = []
+        self.col1       = 0
+        self.col2       = 1
+        self.col3       = None
+        self.col4       = None
         self.wr         = 20
         self.title      = None
-        self.oname      = 'SpectralLines'
+        self.oname      = 'Lines'
         self.N          = 6
         self.v          = 0.0
         self.labels     = []
@@ -36,6 +41,10 @@ class lplinterp():
         # Get the command file, and read in each command. Pass it to the interpreter.
         if finput == None:
             finput = raw_input('Please enter a control filename: ')
+
+        if finput == '':
+            print 'No input file, printing Help:'
+
 
         self.commandfile =  open(finput, 'r')
         
@@ -57,49 +66,105 @@ class lplinterp():
         command = line[0].lower()
         instruction = line[1:]
 
-        print 'Command: <%s>\nInstruction: <%s>\n' % (command, instruction)
+        if self.echo == True:
+            print 'Command: <%s>' % (command)
 
-        if command == 'lines':
+        if command == 'echo':
+            self.echo = (instruction[0].lower() == 'on' or instruction[0].lower() == 'true' or int(instruction[0]) == 1)
+
+        elif command == 'lines':
             self.read_lines()
+            if self.echo:
+                print 'Plotting the following lines:'
+                for i in self.lines:
+                    print i
+
+        elif command == 'cols':
+            # Ugly code. Think of a better way of doing this... Might take some work though.
+
+            self.col1, self.col2, self.col3, self.col4 = [None, None, None, None]
+
+            if len(instruction) == 2:
+                self.col1, self.col2 = [int(x) for x in instruction]
+                if self.echo:
+                    print 'Reading data from columns %d and %d' % (self.col1, self.col2)
+
+            elif len(instruction) == 3:
+                self.col1, self.col2, self.col3 = [int(x) for x in instruction]
+                if self.echo:
+                    print 'Reading data from columns %d, %d, %d.' % (self.col1, self.col2, self.col3)
+            
+            elif len(instruction) == 4:
+                self.col1, self.col2, self.col3, self.col4 = [int(x) for x in instruction]
+                if self.echo:
+                    print 'Reading data from columns %d, %d, %d, %d.' % (self.col1, self.col2, self.col3, self.col4)
 
         elif command == 'labels':
             self.read_labels()
+            if self.echo:
+                print "Using the following labels:"
+                for i in self.labels:
+                    print "<%s>" % i
 
         elif command == 'whitespace':
             self.pad = float(instruction[0])
+            if self.echo:
+                print "Using a left padding space of %.2f%%" % self.pad 
 
         elif command == 'offset':
             self.offset = float(instruction[0])
+            if self.echo:
+                print "Offsetting each subsequent line by %.2f" % self.offset
 
         elif command == 'text_shift':
             self.text_shift = float(instruction[0])
+            if self.echo:
+                print "Text location (fractional distance from the left) -- %.2f" % self.text_shift
 
         elif command == 'out':
             if instruction[0][0] == 'y' or instruction[0][0] == 'n':
                 self.out = instruction[0][0]
             else:
                 print 'Invalid <out> argument! [y/n]'
+            if self.echo:
+                print "Print to file? %1s" % self.out
 
         elif command == 'spec':
             self.fname.append(instruction[0])
+            if self.echo:
+                print "Reading data from the following files:"
+                for i in self.fname:
+                    print "<%s>" % i
 
         elif command == 'figsize':
             self.figsize = [float(instruction[0]), float(instruction[1])]
+            if self.echo:
+                print "Figure will be %.2f inches wide and %.2f inches tall." % (self.figsize[0], self.figsize[1])
 
         elif command == 'wr':
             self.wr = float(instruction[0])
+            if self.echo:
+                print "Snipping data from %.2fA about the lines"
 
         elif command == 'title':
             self.title = ' '.join(instruction)
+            if self.echo:
+                print "Figure Title: <%s>" % self.title
 
         elif command == 'oname':
             self.oname = '_'.join(instruction)
+            if self.echo:
+                print "Writing to %s.pdf" % self.oname
 
         elif command == 'n':
             self.N = int(instruction[0])
+            if self.echo:
+                print "Plotting %d lines per figure" % self.N
 
         elif command == 'v':
             self.v = float(instruction[0])
+            if self.echo:
+                print 'Velocity correction: %.2f' % self.v
 
         elif command == 'run':
             self.run()
@@ -110,8 +175,13 @@ class lplinterp():
         else:
             print 'ERROR: Command <%s %s> is not valid.' % (command, ''.join(line))
 
+        if self.echo:
+            print '\n'
+
     def read_lines(self):
-        # Read in numbers from the file until we find an empty line
+        # Read in numbers from the file until we find an empty line.
+        # Clear the current lines and labels lists.
+        self.lines = []
         line = self.commandfile.readline().strip()
         self.i += 1 
         while line != '':
@@ -121,41 +191,56 @@ class lplinterp():
                 self.i += 1
             except ValueError:
                 print 'There was an error reading in your line wavelengths!'
-                print 'Error was on line %d\n' % self.i
+                print 'Error was on line %d\n%s' % (self.i, line)
                 break
-        self.labels = [None for x in self.lines]
 
     def read_labels(self):
         # Read in the labels from the file until we find an empty line
         line = self.commandfile.readline().strip()
         self.i += 1
         labels = []
-        while line != '':
-            try:
-                labels.append(line)
-                line = self.commandfile.readline().strip()
-                self.i += 1
-            except ValueError:
-                break
 
-        for j in range(len(self.lines)):
-            self.labels[j] = labels[j]
+        while line != '':
+            labels.append(line)
+            line = self.commandfile.readline().strip()
+            self.i += 1
+
+        self.labels = labels
+
+    def check_lines_labels(self):
+        # If lines and labels aren't the same shape, alter the labels list to be so.
+
+        labels = [None for x in self.lines] # List of the shape lines
+
+        i = len(self.lines)
+        for j in range(i):
+            labels[j] = self.labels[j]
+
 
     def run(self):
         # Run the plotting function
-        # Generate the plots.
-        # generate_plots(Hlines, fname, oname=oname+'_Balmer-', N=5, labels=Hlabels, out=out)
-        # print 'Done balmer...'
+        
+        # If we only have one fname, we can use a different reading function. 
+        ## I don't know why I did this since it's not any faster, but it's not any slower either so whatever.
         if len(self.fname) == 1:
             fname = str(fname[0])
+        # Check that an fname has been given at all:
+        if self.fname[0] == '':
+            print 'Please supply a spectrum file!!'
+            exit()
 
-        # print('lpl.generate_plots(lines=%s, fname=%s, wr=%s, title=%s, oname=%s, N=%s, labels=%s, offest=%s, text_shift=%s, out=%s, pad=%s, v=%s)' % 
-        #                         (self.lines, self.fname, self.wr, self.title, self.oname, self.N, 
-        #                             self.labels, self.offset, self.text_shift, self.out, self.pad, self.v))
+        # Check the line/label lists
+        self.check_lines_labels()
+
+        if self.echo:
+            print('lpl.generate_plots(lines=%s, fname=%s, wr=%s, title=%s, oname=%s, N=%s, labels=%s, offest=%s, text_shift=%s, out=%s, pad=%s, v=%s)' % 
+                                    (self.lines, self.fname, self.wr, self.title, self.oname, self.N, 
+                                        self.labels, self.offset, self.text_shift, self.out, self.pad, self.v))
 
         # Pass all the arguments we've collected over to the module
         lpl.generate_plots(self.lines, self.fname, wr=self.wr, title=self.title, oname=self.oname, 
                             N=self.N, labels=self.labels, offset=self.offset, text_shift=self.text_shift, 
-                            out=self.out, figsize=self.figsize, pad=self.pad, v=self.v)
+                            out=self.out, figsize=self.figsize, pad=self.pad, v=self.v, col1=self.col1, 
+                            col2=self.col2, col3=self.col3, col4=self.col4)
 
-active = lplinterp('input.pwl')
+active = lplinterp()
